4d9f559a3ac4292a87ba99470e03d35a68ef27f4
shumei <cyhlbh2010@gmail.com>
2023-09-19 16:07:22 +0800

    完善员工添加

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,9 +1,11 @@
 package com.sky.service;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 
 public interface EmployeeService {
+
 
     /**
      * 员工登录
@@ -11,5 +13,11 @@ public interface EmployeeService {
      * @return
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+     void save(EmployeeDTO employeeDTO);
 
 }
